99ba82946e6b92db3eee20c0ea354a5a48048a75
Savannahj17 <88413820+Savannahj17@users.noreply.github.com>
2021-12-24 21:33:10 -0500

    Delete source/repos/RestaurantRader directory

--- source/repos/RestaurantRader/RestaurantRader/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace RestaurantRader.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- source/repos/RestaurantRader/RestaurantRader/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RestaurantRader.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
